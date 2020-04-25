8c4c7b7c161119c8dcdc1f8640b1c6c9baf8684f
reynoldb02 <32553180+reynoldb02@users.noreply.github.com>
2020-04-25 01:17:29 -0500

    Update settings.js

--- Epic Modules/trafficplus/settings.js
+++ Epic Modules/trafficplus/settings.js
@@ -1,4 +1,13 @@
 async function main(settings) {
-
+settings.on('submit', function (data) {
+    if (data.myText !== 'Hello') {
+      // We can return a custom Error object like so to prevent the settings from being saved
+      // The input will automatically be highlighted for the user
+      return {
+        error: new Error('Please make this field exactly match: "Hello"'),
+        input: 'myText'
+      }
+    }
+  })
 }
 module.exports = main;
